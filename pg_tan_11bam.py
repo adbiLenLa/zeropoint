b86712a75f738b470a543bd42f1bf251ce05bfba
john david jones <79369410+adbiLenLa@users.noreply.github.com>
2023-05-10 18:24:54 +0300

    Add files via upload

---
+++ pg_tan_11bam.py
@@ -0,0 +1,54 @@
+#11bam/3xs:0z:vV:::
+#
+#    john.david jones
+#    ozazL
+#    vanhan vaasan sairaaLa
+#------------
+import pygame as pg 
+from pygame.locals import *
+from random import randint
+import math
+
+exec(open('azq_zdd_11ba9.py').read())
+
+fps = pg.time.Clock()
+FPS = 60
+
+pg.init()
+sc_w = 418
+sc_h = 418
+_ds = pg.display.set_mode((sc_w, sc_h), 0, 32)
+pg.display.set_caption('pg_tan_11bam')
+b_x = 0
+b_xh = sc_w // 2
+b_yh = sc_h // 2
+d_aL = 0.0
+d_inc = 0.1
+while True:
+    for event in pg.event.get():
+        if event.type == QUIT:
+            pg.quit()
+    b_x = -b_xh
+    _ds.fill((0,0,0))
+    d_aL = 0.0 
+    while b_x < b_xh:
+        d_aL += (math.pi * 2) / sc_w
+        
+        b_y = a7d(math.sin(d_aL), math.cos(d_aL) )* 108
+        #b__x = math.cos(d_aL) * 108
+        #r_cLr = (urI(256), urI(256), urI(256))
+        L__ = _urL(26, 256)
+        r_cLr = (L__[6], L__[11], L__[23])
+        #r_pos = (randint(0, 417), randint(0, 417))
+        b_xp = b_x + b_xh
+        b_yp = b_yh - b_y
+        r_pos = (int(b_xp), int(b_yp))
+    
+        _ds.set_at(r_pos, r_cLr)
+    #b_x += (2 * math.pi) / 3600
+        b_x += 1
+    fps.tick(FPS)
+    pg.display.update()
+
+
+
